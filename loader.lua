b99a89b56a52c323dd00732baa9b159686a41125
visionarytools <austinparks@mail.com>
2026-07-20 17:53:57 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,27 +1,27 @@
 local Prepass = loadstring(game:HttpGet("https://raw.githubusercontent.com/visionarytools/tool1/refs/heads/main/source/prepass.lua", true))()
 
 local Options = {
-    ReadMe = false,
-    SafeMode = true,
+    ReadMe = true, -- false
+    SafeMode = false, -- false
     BoostFPS = true,
-    ShutdownWhenDone = true,
+    ShutdownWhenDone = false, -- true
     mode = "full",
     Decompile = true,
     DecompileTimeout = -1,
-    SaveBytecode = true,
+    SaveBytecode = false, -- true
     DecompileIgnore = {},
     IgnoreList = {},
-    NilInstances = true,
+    NilInstances = false, -- true
     SavePlayerGui = true,
     SetStreaming = false,
-    DecompilePrepass = false,
+    DecompilePrepass = true, -- false
 }
 
 local PrepassOptions = {
     RequestsPerMinute = 1495,
     MaxInFlight       = 30,
     ApiUrl            = "https://api.lua.expert/decompile",
-    Verbose           = true,
+    Verbose           = false, -- true
     SkipPrepass       = false,
     SkipSaveInstance  = false,
     UssiRepoURL       = "https://raw.githubusercontent.com/visionarytools/tool1/refs/heads/main/source/",
